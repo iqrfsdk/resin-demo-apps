15d0120284a3f077e7d369514d05f8078d815a76
Rostislav Spinar <rostislav.spinar@microrisc.com>
2016-09-25 23:48:52 +0200

    Update run script

--- resin-open-gateway/bin/run-og.sh
+++ resin-open-gateway/bin/run-og.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd bin/
-
-java -Djava.library.path=natives/armhf/osgi \
-     -Dlogback.configurationFile=config/logback/logback.xml \
-     -cp open-gateway-0.1.0.jar: \
+java -Djava.library.path=bin/natives/armhf/osgi \
+     -Dlogback.configurationFile=bin/config/logback/logback.xml \
+     -cp bin/open-gateway-0.1.0.jar: \
      com.microrisc.opengateway.apps.monitoring.OpenGatewayApp
 #    com.microrisc.opengateway.apps.monitoring.OpenGatewayApp > oga.log 2>&1
 
